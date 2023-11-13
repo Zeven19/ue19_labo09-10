fa6a5fb5d547014256ea50536e6a1e85b40a2f63
Zeven19 <120576639+Zeven19@users.noreply.github.com>
2023-11-13 15:24:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import requests
 
+url = "https://rickandmortyapi.com/api/character/"
+
+reponse = requests.get(url)
+
+if reponse.status_code == 200:
+    donnee = reponse.json()
+    print(donnee)
+else:
+    print("Erreur")
